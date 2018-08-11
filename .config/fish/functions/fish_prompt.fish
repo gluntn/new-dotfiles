30ba6bd9eb1a73e4cacda405fe86b1e0945ed120
William S Østensen <wostensen96@gmail.com>
2018-08-11 14:58:31 +0200

    edited my prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
-	set_color -o 1fd; printf (pwd)
-	set_color 1fd; printf "\n\u21d2"
+	set_color -o 1fd; printf "\u250c "(pwd)
+	set_color 1fd; printf "\n\u2514\u2500"
 	set_color normal; printf " "
 end
