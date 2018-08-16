1c357cb7b38a49d3566267f2ca05879eef1e9cdf
William S Østensen <wostensen96@gmail.com>
2018-08-17 00:55:44 +0200

    some new updates!

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
-	set_color -o 1fd; printf "\u250c "(pwd)
+	set_color -o 1fd; printf "\u250c\u2524"(pwd)"\u2502"
 	set_color 1fd; printf "\n\u2514\u2500"
 	set_color normal; printf " "
 end
