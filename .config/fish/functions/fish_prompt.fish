51b72d1f7f6006ae18179f09bdf4378eb2634881
William S Østensen <wostensen96@gmail.com>
2018-08-26 13:51:25 +0200

    a lot of changes

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
-	set_color -o 1f9; printf "\u250c "(pwd)(__fish_git_prompt)
+	set_color --bold 1f9; printf "\u250c "(pwd)(__fish_git_prompt)
 	set_color 1f9; printf "\n\u2514\u2500"
 	set_color normal; printf " "
 end
